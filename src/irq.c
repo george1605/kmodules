a190b625d818087de2f8967cd232c879b4927582
george1605 <32997165+george1605@users.noreply.github.com>
2022-02-12 12:07:43 +0200

    Update irq.c

--- src/irq.c
+++ src/irq.c
@@ -2,6 +2,9 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
+#define KBD_IRQ 1
+#define DISK_IRQ 14
+#define MOUSE_IRQ 12
 
 typedef irqreturn_t(*handler)(int, void*, struct pt_regs*);
 static void* devid;
